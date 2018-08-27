9d9ef141136fe142811a6618726147463a42d531
atlantis0617 <973549268@qq.com>
2018-08-27 10:10:38 +0800

    rabbitmq初始化

--- src/test/java/com/example/demo/RabbitMqHelloApplicationTests.java
+++ src/test/java/com/example/demo/RabbitMqHelloApplicationTests.java
@@ -6,19 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.yhy.rabbitmq.OrderSender;
+import com.yhy.rabbitmq.ProductSender;
 import com.yhy.rabbitmq.RabbitMqHelloApplication;
-import com.yhy.rabbitmq.Sender;
+import com.yhy.rabbitmq.UserSender;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=RabbitMqHelloApplication.class)
 public class RabbitMqHelloApplicationTests {
 	
 	@Autowired
-	private Sender sender;
+	private UserSender sender;
+	@Autowired
+	private ProductSender psender;
+	@Autowired
+	private OrderSender osender;
 
 	@Test
 	public void send() {
 		this.sender.send();
+		this.psender.send();
+		this.osender.send();
 	}
 
 }
